58f59c72a5cb8ce062b55eb4d65a2a1461bcf40f
Pau Fernández <pau.fernandez@upc.edu>
2024-10-02 12:01:38 +0200

    Use "fetch" instead of axios
    + Prettier config

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "@hey-api/openapi-ts"
 
 export default defineConfig({
-    client: "axios",
+    client: "fetch",
     input: "https://api.jutge.org/v1/OpenAPI.json",
     base: "https://api.jutge.org",
     output: "src/client",
