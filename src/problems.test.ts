83b0a2488f6795af7d6c7812b2d08c87927ad491
Pau Fernández <pau.fernandez@upc.edu>
2024-10-03 11:07:13 +0200

    Added FIXMEs

--- src/problems.test.ts
+++ src/problems.test.ts
@@ -5,12 +5,14 @@ import { describeWithToken } from "./test-utils"
 describeWithToken("Problems", () => {
     const jom = new JOM()
 
+    // FIXME(pauek): The API is giving internal error for now
     test.skip("problems.all()", async () => {
         const problems = await jom.problems.all()
         console.log("Number of problems:", Object.keys(problems).length)
         expect(Object.keys(problems).length).toBeGreaterThan(0)
     })
 
+    // FIXME(pauek): The API is giving internal error for now
     test.skip("problems.get()", async () => {
         const problems = await jom.problems.all()
         const problemId = Object.keys(problems)[0]
